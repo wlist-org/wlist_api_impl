225d59cedc2cf2349e9474c0cb18932b96390a2b
xuxiaocheng <2762267080@qq.com>
2024-02-13 00:33:45 +0800

    feat(event): 添加存储登录事件

--- src/com/xuxiaocheng/wlist/api/core/broadcast/events/Event.java
+++ src/com/xuxiaocheng/wlist/api/core/broadcast/events/Event.java
@@ -4,5 +4,5 @@ package com.xuxiaocheng.wlist.api.core.broadcast.events;
  * An interface that represents an event.
  */
 public sealed interface Event permits ServerCloseEvent,
-        StorageAddEvent, StorageRemoveEvent {
+        StorageAddEvent, StorageRemoveEvent, StorageLoginEvent {
 }
