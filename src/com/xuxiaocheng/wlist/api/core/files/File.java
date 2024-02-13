43bb48c77e17291262a5eda47b54728163208372
xuxiaocheng <2762267080@qq.com>
2024-02-13 23:33:04 +0800

    refactor(api): 拆分refresh接口

--- src/com/xuxiaocheng/wlist/api/core/files/File.java
+++ src/com/xuxiaocheng/wlist/api/core/files/File.java
@@ -34,49 +34,4 @@ public enum File {;
      * @return a future, with the optional file/directory information.
      */
     public static CompletableFuture<Optional<FileInformation>> get(final CoreClient client, final String token, final FileLocation file) { return Main.future(); }
-
-    /**
-     * Refresh the directory.
-     * Note the refresh token will lock the directory until it is canceled/finished.
-     * @param client the core client.
-     * @param token the core token.
-     * @param directory the location of the directory. {@code assert directory.isDirectory;}
-     * @return a future, with the refresh token.
-     * @see com.xuxiaocheng.wlist.api.core.files.exceptions.FileNotFoundException
-     */
-    public static CompletableFuture<RefreshToken> refresh(final CoreClient client, final String token, final FileLocation directory) { return Main.future(); }
-
-    /**
-     * Cancel a refresh.
-     * @param client the core client.
-     * @param token the refresh token.
-     * @return a future.
-     */
-    public static CompletableFuture<Void> cancelRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
-
-    /**
-     * Confirm a refresh.
-     * @param client the core client.
-     * @param token the refresh token.
-     * @return a future. It will be complete after finishing, even it's paused.
-     */
-    public static CompletableFuture<Void> confirmRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
-
-    /**
-     * Pause a refresh.
-     * Note that if refresh has been paused, the method will return normally.
-     * @param client the core client.
-     * @param token the refresh token.
-     * @return a future.
-     */
-    public static CompletableFuture<Void> pauseRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
-
-    /**
-     * Resume a refresh
-     * Note that if refresh has been running, the method will return normally.
-     * @param client the core client.
-     * @param token the refresh token.
-     * @return a future.
-     */
-    public static CompletableFuture<Void> resumeRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
 }
---
+++ src/com/xuxiaocheng/wlist/api/core/files/Refresh.java
@@ -0,0 +1,57 @@
+package com.xuxiaocheng.wlist.api.core.files;
+
+import com.xuxiaocheng.wlist.api.Main;
+import com.xuxiaocheng.wlist.api.core.CoreClient;
+import com.xuxiaocheng.wlist.api.core.files.tokens.RefreshToken;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * The core refresh API.
+ */
+public enum Refresh {;
+    /**
+     * Refresh the directory.
+     * Note the refresh token will lock the directory until it is canceled/finished.
+     * @param client the core client.
+     * @param token the core token.
+     * @param directory the location of the directory. {@code assert directory.isDirectory;}
+     * @return a future, with the refresh token.
+     * @see com.xuxiaocheng.wlist.api.core.files.exceptions.FileNotFoundException
+     */
+    public static CompletableFuture<RefreshToken> refresh(final CoreClient client, final String token, final FileLocation directory) { return Main.future(); }
+
+    /**
+     * Cancel a refresh.
+     * @param client the core client.
+     * @param token the refresh token.
+     * @return a future.
+     */
+    public static CompletableFuture<Void> cancelRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
+
+    /**
+     * Confirm a refresh.
+     * @param client the core client.
+     * @param token the refresh token.
+     * @return a future. It will be complete after finishing, even it's paused.
+     */
+    public static CompletableFuture<Void> confirmRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
+
+    /**
+     * Pause a refresh.
+     * Note that if refresh has been paused, the method will return normally.
+     * @param client the core client.
+     * @param token the refresh token.
+     * @return a future.
+     */
+    public static CompletableFuture<Void> pauseRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
+
+    /**
+     * Resume a refresh
+     * Note that if refresh has been running, the method will return normally.
+     * @param client the core client.
+     * @param token the refresh token.
+     * @return a future.
+     */
+    public static CompletableFuture<Void> resumeRefresh(final CoreClient client, final RefreshToken token) { return Main.future(); }
+}
