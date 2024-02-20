50a5d3f32482a2fb22ed85038501ba980684a1c2
xuxiaocheng <2762267080@qq.com>
2024-02-20 18:57:16 +0800

    test(lib): 测试版本字符串

---
+++ tests/com/xuxiaocheng/wlist/api/Tester.java
@@ -0,0 +1,13 @@
+package com.xuxiaocheng.wlist.api;
+
+import org.junit.jupiter.api.BeforeAll;
+
+public class Tester {
+    static {
+        System.loadLibrary("wlist");
+    }
+
+    @BeforeAll
+    public static void initialize() {
+    }
+}
---
+++ tests/com/xuxiaocheng/wlist/api/common/MarkTest.java
@@ -0,0 +1,37 @@
+package com.xuxiaocheng.wlist.api.common;
+
+import com.xuxiaocheng.wlist.api.Tester;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MarkTest {
+    @BeforeAll
+    public static void initialize() {
+        Tester.initialize();
+    }
+
+    @Test
+    @SuppressWarnings("ConstantAssertArgument")
+    public void impl() {
+        Assertions.assertTrue(Marks.WITH_IMPL);
+    }
+
+    @Test
+    public void commonAPI() {
+        Assertions.assertEquals("1.0.0", Marks.COMMON_API_VERSION);
+        Assertions.assertEquals("0.1.0", Marks.COMMON_IMPL_VERSION);
+    }
+
+    @Test
+    public void coreAPI() {
+        Assertions.assertEquals("1.0.1", Marks.CORE_API_VERSION);
+        Assertions.assertEquals("0.1.0", Marks.CORE_IMPL_VERSION);
+    }
+
+    @Test
+    public void webAPI() {
+        Assertions.assertEquals("0.2.0", Marks.WEB_API_VERSION);
+        Assertions.assertEquals("0.1.0", Marks.WEB_IMPL_VERSION);
+    }
+}
