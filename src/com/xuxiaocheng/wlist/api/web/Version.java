1e0c8ddfcf6c3305cbf11246ccbb28e1d8c4882b
xuxiaocheng <2762267080@qq.com>
2024-02-21 23:04:06 +0800

    feat(api): 添加检查更新接口

---
+++ src/com/xuxiaocheng/wlist/api/web/Version.java
@@ -0,0 +1,43 @@
+package com.xuxiaocheng.wlist.api.web;
+
+import com.xuxiaocheng.wlist.api.Main;
+import com.xuxiaocheng.wlist.api.common.Stable;
+import com.xuxiaocheng.wlist.api.common.StableModule;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * The web version API.
+ */
+@Stable(since = "1.1.0", module = StableModule.Web)
+public enum Version {
+    /**
+     * The current version is the latest and needn't upgrade.
+     */
+    Latest,
+
+    /**
+     * The current version is not the latest but still can be used.
+     */
+    Updatable,
+
+    /**
+     * The current version shouldn't be used and must upgrade.
+     */
+    Unavailable,
+    ;
+
+    /**
+     * Check the core version.
+     * @return a future, with the update information.
+     * @see com.xuxiaocheng.wlist.api.common.Marks#CORE_IMPL_VERSION
+     */
+    public static CompletableFuture<Version> checkCore() { return Main.future(); }
+
+    /**
+     * Check the web version.
+     * @return a future, with the update information.
+     * @see com.xuxiaocheng.wlist.api.common.Marks#WEB_IMPL_VERSION
+     */
+    public static CompletableFuture<Version> checkWeb() { return Main.future(); }
+}
