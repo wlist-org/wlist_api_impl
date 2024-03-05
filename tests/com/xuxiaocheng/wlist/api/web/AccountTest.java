8d8c5ca330d2b8114b75890eaa94629b57aada78
xuxiaocheng <2762267080@qq.com>
2024-03-05 15:46:08 +0800

    test(lib): 测试web.Account.logout接口

--- tests/com/xuxiaocheng/wlist/api/web/AccountTest.java
+++ tests/com/xuxiaocheng/wlist/api/web/AccountTest.java
@@ -2,6 +2,7 @@ package com.xuxiaocheng.wlist.api.web;
 
 import com.xuxiaocheng.wlist.api.Tester;
 import com.xuxiaocheng.wlist.api.common.NetworkFuture;
+import com.xuxiaocheng.wlist.api.common.exceptions.TokenExpiredException;
 import com.xuxiaocheng.wlist.api.web.exceptions.MatchFrequencyControlException;
 import com.xuxiaocheng.wlist.api.web.exceptions.PasswordNotMatchedException;
 import org.junit.jupiter.api.Assertions;
@@ -21,7 +22,7 @@ public class AccountTest {
 
     @Nested
     @DisplayName("login")
-    public class LoginMethod {
+    public class Login {
         @Test
         @DisplayName("right password")
         public void loginSuccess() {
@@ -46,7 +47,7 @@ public class AccountTest {
         @DisplayName("hacking")
         public void loginHacking() {
             final ArrayDeque<NetworkFuture<String>> futures = new ArrayDeque<>();
-            for (int i = 0; i < 16; ++i)
+            for (int i = 0; i < 8; ++i)
                 futures.add(Account.login("1", "wrong pw"));
             while (!futures.isEmpty())
                 try (final NetworkFuture<String> future = futures.remove()) {
@@ -55,6 +56,40 @@ public class AccountTest {
                         return;
                 }
             Assertions.fail("Should throw MatchFrequencyControlException.");
+        }
+    }
+
+    @Nested
+    @DisplayName("logout")
+    public class Logout {
+        @Test
+        @DisplayName("normal token")
+        public void logoutNormally() {
+            final String token;
+            try (final NetworkFuture<String> future = Account.login("1", "change me")) {
+                token = Assertions.assertDoesNotThrow(() -> future.get());
+            }
+
+            try (final NetworkFuture<Void> future = Account.logout(token)) {
+                Assertions.assertDoesNotThrow(() -> future.get());
+            }
+        }
+
+        @Test
+        @DisplayName("blank token")
+        public void logoutEmpty() {
+            try (final NetworkFuture<Void> future = Account.logout("")) {
+                final ExecutionException exception = Assertions.assertThrowsExactly(ExecutionException.class, future::get);
+                Assertions.assertInstanceOf(TokenExpiredException.class, exception.getCause());
+            }
+        }
+
+        @Test
+        @DisplayName("wrong token")
+        public void logoutWrong() {
+            try (final NetworkFuture<Void> future = Account.logout("wrong_token.just-a_test")) {
+                Assertions.assertDoesNotThrow(() -> future.get());
+            }
         }
     }
 }
