6419e80c62453f1453ec09bdc8e9c557824ea7fb
xuxiaocheng <2762267080@qq.com>
2024-03-06 14:10:57 +0800

    feat(exception): 添加IncorrectArgumentException异常

---
+++ src/com/xuxiaocheng/wlist/api/common/exceptions/IncorrectArgumentException.java
@@ -0,0 +1,19 @@
+package com.xuxiaocheng.wlist.api.common.exceptions;
+
+import java.io.Serial;
+
+/**
+ * Thrown if you pass an incorrect argument to a method.
+ * Please check the javadoc to ensure the argument you've passed is correct.
+ */
+public class IncorrectArgumentException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 2376130325270336896L;
+
+    /**
+     * Internal constructor.
+     */
+    protected IncorrectArgumentException(final String message) {
+        super(message);
+    }
+}
