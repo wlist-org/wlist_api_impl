976ae35511a5283d6ce872580ddadb239ff4f284
xuxiaocheng <2762267080@qq.com>
2024-03-28 12:03:51 +0800

    feat(api): 优化异常构造函数

--- src/com/xuxiaocheng/wlist/api/common/exceptions/NetworkException.java
+++ src/com/xuxiaocheng/wlist/api/common/exceptions/NetworkException.java
@@ -16,4 +16,13 @@ public class NetworkException extends InternalException {
     private NetworkException(final String message) {
         super(message);
     }
+
+    /**
+     * Internal constructor.
+     * @param message error message.
+     * @param cause error cause.
+     */
+    private NetworkException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
