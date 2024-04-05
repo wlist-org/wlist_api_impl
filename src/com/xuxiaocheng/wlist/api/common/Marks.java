f0ac9f802543f7a238e28c1ac86949e38597d4e0
xuxiaocheng <2762267080@qq.com>
2024-04-05 10:29:37 +0800

    build(lib): Release 1.9.0

--- src/com/xuxiaocheng/wlist/api/common/Marks.java
+++ src/com/xuxiaocheng/wlist/api/common/Marks.java
@@ -29,7 +29,7 @@ public final class Marks {
     /**
      * The core api version.
      */
-    public static final String CORE_API_VERSION = "1.6.0";
+    public static final String CORE_API_VERSION = "1.7.0";
     /**
      * The core api impl version.
      */
