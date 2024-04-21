e97684c48dde896dea6ecd4cbc6b1e429ba47a2d
xuxiaocheng <2762267080@qq.com>
2024-04-21 23:56:41 +0800

    feat(server): 添加序列化

--- src/com/xuxiaocheng/wlist/api/core/trashes/options/SearchOptions.java
+++ src/com/xuxiaocheng/wlist/api/core/trashes/options/SearchOptions.java
@@ -2,7 +2,10 @@ package com.xuxiaocheng.wlist.api.core.trashes.options;
 
 import com.xuxiaocheng.wlist.api.common.Recyclable;
 import com.xuxiaocheng.wlist.api.core.files.options.Pattern;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 /**
@@ -12,4 +15,14 @@ import java.io.Serializable;
  */
 public record SearchOptions(String keyword, Pattern pattern)
         implements Serializable, Recyclable {
+    public static void serialize(final SearchOptions self, final MessagePacker packer) throws IOException {
+        packer.packString(self.keyword);
+        packer.packString(self.pattern.name());
+    }
+
+    public static SearchOptions deserialize(final MessageUnpacker unpacker) throws IOException {
+        final String keyword = unpacker.unpackString();
+        final Pattern pattern = Pattern.valueOf(unpacker.unpackString());
+        return new SearchOptions(keyword, pattern);
+    }
 }
