baef3657d5d68aabf6ed9997d182829656211eb2
xuxiaocheng <2762267080@qq.com>
2024-05-15 13:38:22 +0800

    feat(api): information time支持unknown

--- src/com/xuxiaocheng/wlist/api/core/trashes/information/TrashInformation.java
+++ src/com/xuxiaocheng/wlist/api/core/trashes/information/TrashInformation.java
@@ -11,9 +11,9 @@ import java.time.Instant;
  * @param name the file/directory name.
  * @param isDirectory whether a directory or a file.
  * @param size the file/directory size. (-1 means unknown.)
- * @param createTime the time when created.
- * @param updateTime the time when updated.
- * @param trashTime the time when trashed.
+ * @param createTime the nullable time when created. (null means unknown.)
+ * @param updateTime the nullable time when updated. (null means unknown.)
+ * @param trashTime the nullable time when trashed. (null means unknown.)
  */
 public record TrashInformation(long id, String name, boolean isDirectory, long size,
                                Instant createTime, Instant updateTime, Instant trashTime)
