4450c3abb48962236632f415a785938094bf5557
xuxiaocheng <2762267080@qq.com>
2024-05-15 19:03:03 +0800

    feat(server): 添加ShareInformation序列化方法

--- src/com/xuxiaocheng/wlist/api/core/files/information/ShareInformation.java
+++ src/com/xuxiaocheng/wlist/api/core/files/information/ShareInformation.java
@@ -1,7 +1,10 @@
 package com.xuxiaocheng.wlist.api.core.files.information;
 
 import com.xuxiaocheng.wlist.api.common.Recyclable;
+import org.msgpack.core.MessagePacker;
+import org.msgpack.core.MessageUnpacker;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.time.Instant;
 
@@ -13,4 +16,21 @@ import java.time.Instant;
  */
 public record ShareInformation(String id, String optionalPassword, Instant expire)
         implements Serializable, Recyclable {
+    public static void serialize(final ShareInformation self, final MessagePacker packer) throws IOException {
+        packer.packString(self.id);
+        if (self.optionalPassword == null)
+            packer.packBoolean(false);
+        else {
+            packer.packBoolean(true);
+            packer.packString(self.optionalPassword);
+        }
+        packer.packTimestamp(self.expire);
+    }
+
+    public static ShareInformation deserialize(final MessageUnpacker unpacker) throws IOException {
+        final String id = unpacker.unpackString();
+        final String optionalPassword = unpacker.unpackBoolean() ? unpacker.unpackString() : null;
+        final Instant expire = unpacker.unpackTimestamp();
+        return new ShareInformation(id, optionalPassword, expire);
+    }
 }
