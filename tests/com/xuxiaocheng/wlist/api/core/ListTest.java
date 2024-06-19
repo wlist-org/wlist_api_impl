efe4e9187819600a1816e9862d2d61a05ab44095
xuxiaocheng <2762267080@qq.com>
2024-06-19 16:39:06 +0800

    test(storage): 测试get

--- tests/com/xuxiaocheng/wlist/api/core/ListTest.java
+++ tests/com/xuxiaocheng/wlist/api/core/ListTest.java
@@ -6,6 +6,8 @@ import com.xuxiaocheng.wlist.api.common.either.Either;
 import com.xuxiaocheng.wlist.api.core.files.File;
 import com.xuxiaocheng.wlist.api.core.files.FileLocation;
 import com.xuxiaocheng.wlist.api.core.files.confirmations.RefreshConfirmation;
+import com.xuxiaocheng.wlist.api.core.files.exceptions.FileNotFoundException;
+import com.xuxiaocheng.wlist.api.core.files.information.FileDetailsInformation;
 import com.xuxiaocheng.wlist.api.core.files.information.FileListInformation;
 import com.xuxiaocheng.wlist.api.core.files.options.Filter;
 import com.xuxiaocheng.wlist.api.core.files.options.ListFileOptions;
@@ -15,17 +17,23 @@ import com.xuxiaocheng.wlist.api.core.types.LanzouTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.ClassOrderer;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 @DisplayName("files - List")
 @ExtendWith(Basic.ClientArguments.class)
+@TestClassOrder(ClassOrderer.OrderAnnotation.class)
 public abstract class ListTest {
     protected final long storage;
     protected final long root;
@@ -57,17 +65,49 @@ public abstract class ListTest {
         }
     }
 
-    @Test
-    public void list(final CoreClient client, final @Basic.CoreToken String token) {
-        final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(), 0, 10);
+    @Nested
+    @DisplayName("normal")
+    public class Normal {
+        @Test
+        public void list(final CoreClient client, final @Basic.CoreToken String token) {
+            final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(), 0, 10);
 
-        final FileLocation root = new FileLocation(this.storage, this.root, true);
-        final FileListInformation list = ListTest.list(client, token, root, options);
-        Assertions.assertEquals(6, list.total());
-        Assertions.assertEquals(6, list.filtered());
-        Assertions.assertEquals(6, list.files().size());
+            final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
+            final FileListInformation list = ListTest.list(client, token, root, options);
+            Assertions.assertEquals(6, list.total());
+            Assertions.assertEquals(6, list.filtered());
+            Assertions.assertEquals(6, list.files().size());
 
-        Client.close(client);
+            Client.close(client);
+        }
+
+        @Test
+        public void limit(final CoreClient client, final @Basic.CoreToken String token) {
+            final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 0, 4);
+            final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
+            final FileListInformation list = ListTest.list(client, token, root, options);
+            Assertions.assertEquals(6, list.total());
+            Assertions.assertEquals(6, list.filtered());
+            Assertions.assertEquals(4, list.files().size());
+            Assertions.assertEquals("chunk.txt", list.files().get(0).name());
+            Assertions.assertEquals("empty", list.files().get(1).name());
+            Assertions.assertEquals("hello", list.files().get(2).name());
+            Assertions.assertEquals("large.txt", list.files().get(3).name());
+            Client.close(client);
+        }
+
+        @Test
+        public void offset(final CoreClient client, final @Basic.CoreToken String token) {
+            final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 4, 10);
+            final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
+            final FileListInformation list = ListTest.list(client, token, root, options);
+            Assertions.assertEquals(6, list.total());
+            Assertions.assertEquals(6, list.filtered());
+            Assertions.assertEquals(2, list.files().size());
+            Assertions.assertEquals("recursion", list.files().get(0).name());
+            Assertions.assertEquals("special", list.files().get(1).name());
+            Client.close(client);
+        }
     }
 
     @Nested
@@ -189,78 +229,105 @@ public abstract class ListTest {
         }
     }
 
-    @Test
-    public void limit(final CoreClient client, final @Basic.CoreToken String token) {
-        final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 0, 4);
-        final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
-        final FileListInformation list = ListTest.list(client, token, root, options);
-        Assertions.assertEquals(6, list.total());
-        Assertions.assertEquals(6, list.filtered());
-        Assertions.assertEquals(4, list.files().size());
-        Assertions.assertEquals("chunk.txt", list.files().get(0).name());
-        Assertions.assertEquals("empty", list.files().get(1).name());
-        Assertions.assertEquals("hello", list.files().get(2).name());
-        Assertions.assertEquals("large.txt", list.files().get(3).name());
-        Client.close(client);
+    public static FileDetailsInformation get(final CoreClient client, final String token, final Long parent, final FileLocation file, final boolean check) {
+        while (true) {
+            try {
+                return File.get(client, token, file, check).get();
+            } catch (final InterruptedException exception) {
+                Assertions.fail(exception);
+            } catch (final ExecutionException exception) {
+                if (parent != null && exception.getCause() instanceof final FileNotFoundException e) {
+                    Assertions.assertEquals(file, e.getLocation());
+                    list(client, token, new FileLocation(file.storage(), parent, true),
+                            new ListFileOptions(Filter.Both, new LinkedHashMap<>(), 0, 0));
+                    continue;
+                }
+                Assertions.fail(exception);
+            }
+        }
     }
 
-    @Test
-    public void offset(final CoreClient client, final @Basic.CoreToken String token) {
-        final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 4, 10);
-        final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
-        final FileListInformation list = ListTest.list(client, token, root, options);
-        Assertions.assertEquals(6, list.total());
-        Assertions.assertEquals(6, list.filtered());
-        Assertions.assertEquals(2, list.files().size());
-        Assertions.assertEquals("recursion", list.files().get(0).name());
-        Assertions.assertEquals("special", list.files().get(1).name());
-        Client.close(client);
+    protected static void assertMd5(final String expected, final String actual) {
+        Assertions.assertTrue(actual == null || Objects.equals(expected, actual), "expected md5 '" + expected + "' != actual '" + actual + '\'');
     }
 
-    @Test
-    @org.junit.jupiter.api.Order(Integer.MAX_VALUE) // only this method indexed all the children.
-    public void all(final CoreClient client, final @Basic.CoreToken String token) {
-        final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 0, 10);
-        final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
-        final FileListInformation list = ListTest.list(client, token, root, options);
-        Assertions.assertEquals(6, list.total());
-        Assertions.assertEquals(6, list.filtered());
-        Assertions.assertEquals(6, list.files().size());
-        Assertions.assertEquals("chunk.txt", list.files().get(0).name());
-        Assertions.assertEquals("empty", list.files().get(1).name());
-        Assertions.assertEquals("hello", list.files().get(2).name());
-        Assertions.assertEquals("large.txt", list.files().get(3).name());
-        Assertions.assertEquals("recursion", list.files().get(4).name());
-        Assertions.assertEquals("special", list.files().get(5).name());
+    @Nested
+    @DisplayName("indexed")
+    @org.junit.jupiter.api.Order(Integer.MAX_VALUE)
+    public class IndexedNested {
+        @Test
+        public void all(final CoreClient client, final @Basic.CoreToken String token) {
+            final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 0, 10);
+            final FileLocation root = new FileLocation(ListTest.this.storage, ListTest.this.root, true);
+            final FileListInformation list = ListTest.list(client, token, root, options);
+            Assertions.assertEquals(6, list.total());
+            Assertions.assertEquals(6, list.filtered());
+            Assertions.assertEquals(6, list.files().size());
+            Assertions.assertEquals("chunk.txt", list.files().get(0).name());
+            Assertions.assertEquals("empty", list.files().get(1).name());
+            Assertions.assertEquals("hello", list.files().get(2).name());
+            Assertions.assertEquals("large.txt", list.files().get(3).name());
+            Assertions.assertEquals("recursion", list.files().get(4).name());
+            Assertions.assertEquals("special", list.files().get(5).name());
 
-        final FileLocation empty = new FileLocation(ListTest.this.storage, list.files().get(1).id(), true);
-        final FileListInformation emptyList = ListTest.list(client, token, empty, options);
-        Assertions.assertEquals(0, emptyList.total());
-        Assertions.assertEquals(0, emptyList.filtered());
-        Assertions.assertEquals(0, emptyList.files().size());
+            final FileLocation empty = new FileLocation(ListTest.this.storage, list.files().get(1).id(), true);
+            final FileListInformation emptyList = ListTest.list(client, token, empty, options);
+            Assertions.assertEquals(0, emptyList.total());
+            Assertions.assertEquals(0, emptyList.filtered());
+            Assertions.assertEquals(0, emptyList.files().size());
 
-        final FileLocation hello = new FileLocation(ListTest.this.storage, list.files().get(2).id(), true);
-        final FileListInformation helloList = ListTest.list(client, token, hello, options);
-        Assertions.assertEquals(1, helloList.total());
-        Assertions.assertEquals(1, helloList.filtered());
-        Assertions.assertEquals(1, helloList.files().size());
-        Assertions.assertEquals("hello.txt", helloList.files().get(0).name());
+            final FileLocation hello = new FileLocation(ListTest.this.storage, list.files().get(2).id(), true);
+            final FileListInformation helloList = ListTest.list(client, token, hello, options);
+            Assertions.assertEquals(1, helloList.total());
+            Assertions.assertEquals(1, helloList.filtered());
+            Assertions.assertEquals(1, helloList.files().size());
+            Assertions.assertEquals("hello.txt", helloList.files().get(0).name());
 
-        final FileLocation recursion = new FileLocation(ListTest.this.storage, list.files().get(4).id(), true);
-        final FileListInformation recursionList = ListTest.list(client, token, recursion, options);
-        Assertions.assertEquals(1, recursionList.total());
-        Assertions.assertEquals(1, recursionList.filtered());
-        Assertions.assertEquals(1, recursionList.files().size());
-        Assertions.assertEquals("inner", recursionList.files().get(0).name());
+            final FileLocation recursion = new FileLocation(ListTest.this.storage, list.files().get(4).id(), true);
+            final FileListInformation recursionList = ListTest.list(client, token, recursion, options);
+            Assertions.assertEquals(1, recursionList.total());
+            Assertions.assertEquals(1, recursionList.filtered());
+            Assertions.assertEquals(1, recursionList.files().size());
+            Assertions.assertEquals("inner", recursionList.files().get(0).name());
 
-        final FileLocation inner = new FileLocation(ListTest.this.storage, recursionList.files().get(0).id(), true);
-        final FileListInformation innerList = ListTest.list(client, token, inner, options);
-        Assertions.assertEquals(1, innerList.total());
-        Assertions.assertEquals(1, innerList.filtered());
-        Assertions.assertEquals(1, innerList.files().size());
-        Assertions.assertEquals("recursion.txt", innerList.files().get(0).name());
+            final FileLocation inner = new FileLocation(ListTest.this.storage, recursionList.files().get(0).id(), true);
+            final FileListInformation innerList = ListTest.list(client, token, inner, options);
+            Assertions.assertEquals(1, innerList.total());
+            Assertions.assertEquals(1, innerList.filtered());
+            Assertions.assertEquals(1, innerList.files().size());
+            Assertions.assertEquals("recursion.txt", innerList.files().get(0).name());
 
-        Client.close(client);
+            Client.close(client);
+        }
+
+        @Test
+        public void get(final CoreClient client, final @Basic.CoreToken String token) {
+            final ListFileOptions options = new ListFileOptions(Filter.Both, new LinkedHashMap<>(Map.of(Order.Name, Direction.ASCEND)), 0, 10);
+            final FileListInformation list = ListTest.list(client, token, new FileLocation(ListTest.this.storage, ListTest.this.root, true), options);
+
+            final FileDetailsInformation chunk = ListTest.get(client, token, ListTest.this.root, new FileLocation(ListTest.this.storage, list.files().get(0).id(), false), false);
+            Assertions.assertEquals(list.files().get(0), chunk.basic()); // chunk.txt
+            Assertions.assertEquals(List.of(), chunk.path());
+            ListTest.assertMd5("fc6cb96d6681a62e22a2bbd32e5e0519", chunk.optionalMd5());
+
+            final FileDetailsInformation large = ListTest.get(client, token, ListTest.this.root, new FileLocation(ListTest.this.storage, list.files().get(3).id(), false), false);
+            Assertions.assertEquals(list.files().get(3), large.basic()); // large.txt
+            Assertions.assertEquals(List.of(), large.path());
+            ListTest.assertMd5("a755aeccea7e39b4b2b0fa76dcd1e54b", large.optionalMd5());
+
+            final FileLocation helloLocation = new FileLocation(ListTest.this.storage, list.files().get(2).id(), true);
+            final FileDetailsInformation helloDirectory = ListTest.get(client, token, ListTest.this.root, helloLocation, false);
+            Assertions.assertEquals(list.files().get(2), helloDirectory.basic()); // hello
+            Assertions.assertEquals(List.of(), helloDirectory.path());
+            ListTest.assertMd5(null, helloDirectory.optionalMd5());
+
+            final FileListInformation helloList = ListTest.list(client, token, helloLocation, options);
+
+            final FileDetailsInformation hello = ListTest.get(client, token, list.files().get(2).id(), new FileLocation(ListTest.this.storage, helloList.files().get(0).id(), false), false);
+            Assertions.assertEquals(helloList.files().get(0), hello.basic()); // hello.txt
+            Assertions.assertEquals(List.of("hello"), hello.path());
+            ListTest.assertMd5("fc3ff98e8c6a0d3087d515c0473f8677", hello.optionalMd5());
+        }
     }
 
     public static class LanzouListTest extends ListTest {
