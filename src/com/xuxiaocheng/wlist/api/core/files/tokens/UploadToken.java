7397f48999c450d95878f15dd30d46f88a0f9108
xuxiaocheng <2762267080@qq.com>
2024-06-20 22:46:09 +0800

    feat(common): DownloadToken和UploadToken添加StorageType

--- src/com/xuxiaocheng/wlist/api/core/files/tokens/UploadToken.java
+++ src/com/xuxiaocheng/wlist/api/core/files/tokens/UploadToken.java
@@ -1,6 +1,7 @@
 package com.xuxiaocheng.wlist.api.core.files.tokens;
 
 import com.xuxiaocheng.wlist.api.common.Recyclable;
+import com.xuxiaocheng.wlist.api.core.storages.types.StorageType;
 import org.msgpack.core.MessagePacker;
 import org.msgpack.core.MessageUnpacker;
 
@@ -13,15 +14,17 @@ import java.io.Serializable;
  * @param storage the source storage.
  * @param token internal token.
  */
-public record UploadToken(long storage, String token) implements Serializable, Recyclable {
+public record UploadToken(long storage, StorageType<?> type, String token) implements Serializable, Recyclable {
     public static void serialize(final UploadToken self, final MessagePacker packer) throws IOException {
         packer.packLong(self.storage);
+        packer.packString(StorageType.name(self.type));
         packer.packString(self.token);
     }
 
     public static UploadToken deserialize(final MessageUnpacker unpacker) throws IOException {
         final long storage = unpacker.unpackLong();
+        final StorageType<?> type = StorageType.instanceOf(unpacker.unpackString());
         final String token = unpacker.unpackString();
-        return new UploadToken(storage, token);
+        return new UploadToken(storage, type, token);
     }
 }
