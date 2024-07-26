bf466c58825af404f98ab36727867890755cc1ab
xuxiaocheng <2762267080@qq.com>
2024-07-26 14:17:46 +0800

    feat(lib): 优化basic

--- basic/src/lib.rs
+++ basic/src/lib.rs
@@ -1,13 +1,11 @@
-use std::ffi::{c_char, CString};
+use std::ffi::c_char;
 
 #[no_mangle]
-unsafe extern "C" fn allocate_cstring(len: usize) -> *mut c_char {
-    CString::from_vec_with_nul_unchecked(vec![0; len]).into_raw()
+extern "C" fn allocate_cstring(len: usize) -> *mut c_char {
+    vec![0; len].leak().as_mut_ptr()
 }
 
-#[repr(C)]
-pub struct CTime {
-    pub second: i64,
-    pub nanos: i32,
-    pub special: bool,
+#[no_mangle]
+extern "C" fn free_cstring(ptr: *mut c_char, len: usize) {
+    unsafe { Vec::from_raw_parts(ptr, len, len) };
 }
